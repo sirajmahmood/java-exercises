9a65297f1b59e55e1c07ca792e674d3383239162
Siraj Mahmood <sirajmahmood@gmail.com>
2026-08-10 16:59:19 -0400

    done

--- src/main/java/com/amigoscode/_4_datastructures/_1_arrays/ArrayBasics.java
+++ src/main/java/com/amigoscode/_4_datastructures/_1_arrays/ArrayBasics.java
@@ -10,30 +10,39 @@ public class ArrayBasics {
     public static void main(String[] args) {
 
         // TODO: 1 - Create an int array of size 5
+        int[] intAr = new int[5];
 
 
         // TODO: 2 - Fill the array with values 10, 20, 30, 40, 50 using a for loop
         //           (hint: use i * 10 + 10 or similar pattern)
+        for (int i = 0; i < intAr.length; i++) {
+            intAr[i] = (i+1) * 10;
+        }
 
+//        Arrays.stream(intAr).forEach(System.out::println);
 
         // TODO: 3 - Print all elements of the array using Arrays.toString()
+        Arrays.stream(intAr).forEach(System.out::println);
 
 
         // TODO: 4 - Find the maximum value in the array
         //           Iterate through the array and track the largest value found
-
+        int max = Arrays.stream(intAr).max().getAsInt();
+        System.out.println("Max=" + max);
 
         // TODO: 5 - Find the minimum value in the array
         //           Iterate through the array and track the smallest value found
-
+        int min = Arrays.stream(intAr).min().getAsInt();
+        System.out.println("Min=" + min);
 
         // TODO: 6 - Sort the array using Arrays.sort()
         //           Then print the sorted array
-
+        Arrays.stream(intAr).sorted().forEach(System.out::println);
 
         // TODO: 7 - Use Arrays.binarySearch() to find the index of value 30
         //           Note: the array must be sorted before using binarySearch
         //           Print the index where the value was found
-
+        int index = Arrays.binarySearch(intAr, 30);
+        System.out.println("Index=" + index);
     }
 }
--- src/main/java/com/amigoscode/_4_datastructures/_1_arrays/TwoDArrays.java
+++ src/main/java/com/amigoscode/_4_datastructures/_1_arrays/TwoDArrays.java
@@ -3,6 +3,8 @@ package com.amigoscode._4_datastructures._1_arrays;
 // Exercise: Two-Dimensional Arrays
 // Learn how to create and work with 2D arrays (matrices) in Java.
 
+import java.util.Arrays;
+
 public class TwoDArrays {
 
     public static void main(String[] args) {
@@ -16,6 +18,19 @@ public class TwoDArrays {
         //           1 2 3
         //           2 4 6
         //           3 6 9
+        int[][] matrix = new int[3][3];
+
+        for (int row = 0; row < matrix.length; row++) {
+            for (int col = 0; col < matrix[row].length; col++) {
+                matrix[row][col] =  (row+1) * (col + 1);
+            }
+        }
+
+        for (int row = 0; row < matrix.length; row++) {
+            System.out.println(Arrays.toString(matrix[row]));
+        }
+
+        System.out.println(Arrays.deepToString(matrix));
 
 
         // TODO: 3 - Print the matrix in row/column format
@@ -24,15 +39,36 @@ public class TwoDArrays {
 
         // TODO: 4 - Find and print the sum of all elements in the matrix
         //           Use nested loops to accumulate the total
+        int total = 0;
+        for (int[] ints : matrix) {
+            for (int anInt : ints) {
+                total = total + anInt;
+            }
+        }
+        System.out.println("Total = "+total);
 
+        int totals = Arrays.stream(matrix).flatMapToInt(Arrays::stream).sum();
+        System.out.println("Totals = " + totals);
 
         // TODO: 5 - Find and print the sum of row index 1 (the second row: 2, 4, 6)
         //           Iterate through all columns of that specific row
-
+        Arrays.stream(matrix)
+                .mapToInt(row -> Arrays.stream(row).sum())
+                .forEach(System.out::println);
 
         // TODO: 6 - Find and print the sum of the diagonal elements (top-left to bottom-right)
         //           Diagonal elements are where row index == column index
         //           Expected: matrix[0][0] + matrix[1][1] + matrix[2][2] = 1 + 4 + 9 = 14
+        int rowSum = 0;
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                if (i ==j) {
+                    rowSum += matrix[i][j];
+                }
+            }
 
+
+        }
+        System.out.println("Sum: " + rowSum);
     }
 }
